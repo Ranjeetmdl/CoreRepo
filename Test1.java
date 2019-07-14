1f03efda555dd35c7fd4500bdb84fa0effc088ad
Ranjeet <ranjeetmdl@gmail.com>
2019-07-14 17:36:18 +0530

    commiting Test2 new files and modifed Test1 files

--- Test1.java
+++ Test1.java
@@ -1,5 +1,5 @@
 public class Test1{
 public static void main(String[] args){
-	System.out.println("This is Test1");
+	System.out.println("This is Test1:for this you should have knowledge on core java");
 }
 }
