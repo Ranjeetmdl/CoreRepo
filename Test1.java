5025eb9fd78910225e92bb6664c3e46fe97395a4
Ranjeet <ranjeetmdl@gmail.com>
2019-07-15 13:19:11 +0530

    adding Thank You in Test1.java

--- Test1.java
+++ Test1.java
@@ -1,5 +1,6 @@
 public class Test1{
 public static void main(String[] args){
 	System.out.println("This is Test1:for this you should have knowledge on core java");
+	System.out.println("Thank you so much");
 }
 }
