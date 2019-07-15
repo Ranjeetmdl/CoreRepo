66f360f6b607786ce4071ca83f2e414af1c3de96
Ranjeet <ranjeetmdl@gmail.com>
2019-07-15 14:12:49 +0530

    added variable values in sum

--- Test2.java
+++ Test2.java
@@ -4,6 +4,6 @@ public static void main(String[] args){
 	int x=10;
 	int y=20;
 	int sum=x+y;
-	System.out.println("The Sum of two No :"+sum);
+	System.out.println("The Sum of "+x+" & "+y+" is:"+sum);
 }
 }
